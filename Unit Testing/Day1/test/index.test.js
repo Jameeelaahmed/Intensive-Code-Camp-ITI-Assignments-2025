14501768bd832eb54ccef159c80aec80dc53da02
Jameeelaahmed <gamilas320@gmail.com>
2025-06-15 03:11:46 +0300

    Day 1 unit testing finished

---
+++ Unit Testing/Day1/test/index.test.js
@@ -0,0 +1,46 @@
+import { ca } from "date-fns/locale";
+import { capitalizeText, createArray } from "../index.js"
+import * as chai from "chai";
+let { assert, expect } = chai;
+
+// describe("Check capatalize and throw", () => {
+//     it("Capetalaize", () => {
+//         expect(capitalizeText("JAMEELA")).to.equal("JAMEELA");
+//         capitalizeText("Jameela").should.equal("JAMEELA");
+//         assert.equal(capitalizeText("Jameela"), "JAMEELA");
+//     })
+
+//     it("check throw", () => {
+//         expect(() => capitalizeText()).to.throw(TypeError, "parameter should be string");
+//         (() => capitalizeText()).should.throw("parameter should be string");
+//         assert.throws(() => capitalizeText(), "parameter should be string");
+//     })
+// })
+
+
+// *******************
+
+describe("createArray test", () => {
+    // it("return array", () => {
+    //     expect(createArray(3)).to.be.an("array");
+    // });
+
+    // it("return array of length 3 and include 1", () => {
+    //     const arr = createArray(3);
+    //     assert.lengthOf(arr, 3);
+    //     assert.include(arr, 1);
+    // });
+
+    // it("should include 2", () => {
+    //     createArray(5).should.include(2);
+    // });
+
+    // it("delayed test by 5 seconds", (done) => {
+    //     setTimeout(() => {
+    //         expect(createArray(2)).to.deep.equal([0, 1]);
+    //         done();
+    //     }, 5000);
+    // });
+
+    // it("should throw error if input is negative");
+});
